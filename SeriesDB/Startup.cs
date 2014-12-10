ebae5953b58df8c485d26b6014930d06271c10d1
mahammedzkhan <silent785@hotmail.com>
2014-12-10 13:41:24 +0100

    Initial Commit

---
+++ SeriesDB/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SeriesDB.Startup))]
+namespace SeriesDB
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
