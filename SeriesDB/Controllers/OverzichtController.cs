883166948bb73abad7c04f9f8b299b67a50d83f2
mahammedzkhan <silent785@hotmail.com>
2014-12-10 15:30:28 +0100

    Creating a view for create view

--- SeriesDB/Controllers/OverzichtController.cs
+++ SeriesDB/Controllers/OverzichtController.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using SeriesDB.Models;
+using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -13,6 +15,25 @@ namespace SeriesDB.Controllers
         {
             var SerieContext = new SerieContext();
             return View();
+        }
+        public ActionResult Create()
+        {
+            var context = new SerieContext();
+            var viewModel = new SerieDB.Models.SerieViewModel();
+            viewModel.Genres = new List<SelectListItem>();
+            viewModel.Genres.Add(new SelectListItem() { Text = "Selecteer een genre" });
+            foreach (var genre in context.Genres)
+            {
+                viewModel.Genres.Add(new SelectListItem
+                {
+                    Text = genre.Name,
+                    Value = genre.Id.ToString(),
+                    Selected = false
+                });
+            }
+
+
+            return View(viewModel);
         }
     }
 }
