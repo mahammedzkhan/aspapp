066acf42bfd691c833c7d6574f854f7417dca82b
mahammedzkhan <silent785@hotmail.com>
2014-12-10 14:44:48 +0100

    Added overzichtcontroller

---
+++ SeriesDB/Controllers/OverzichtController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SeriesDB.Controllers
+{
+    public class OverzichtController : Controller
+    {
+        // GET: Overzicht
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
