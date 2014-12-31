c51b68d752982de82a30bce0a0b35a927b734eeb
unknown <silent785@hotmail.com>
2014-12-31 11:19:05 +0100

    Added cosmo bootstrap theme

--- SeriesDB/App_Start/BundleConfig.cs
+++ SeriesDB/App_Start/BundleConfig.cs
@@ -24,7 +24,7 @@ namespace SeriesDB
                       "~/Scripts/respond.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
+                      "~/Content/bootstrap.min.css",
                       "~/Content/site.css"));
 
             // Set EnableOptimizations to false for debugging. For more information,
